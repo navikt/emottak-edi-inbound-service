5ea106eed05674fd06544fb86ef5b345fd2132a0
Ivan Skodje <6776092+ivanskodje@users.noreply.github.com>
2025-10-30 10:06:46 +0100

    chore: removed bin folder, added prod config

--- bin/main/no/nav/emottak/edi/inbound/model/AppRec.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.emottak.edi.inbound.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AppRec(
-    val appRecStatus: String = "1",
-    val appRecErrorList: List<AppRecError> = emptyList()
-)
